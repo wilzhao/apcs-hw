64d43a60b9063c6b4015fca1d79ffc3d1c405c22
wilzhao <wzhaomt454@gmail.com>
2014-04-08 06:45:36 -0400

    RPN

--- 13-RPN/RPN.java
+++ 13-RPN/RPN.java
@@ -1,10 +1,19 @@
-
+import java.util.Scanner;
 
 public class RPN{
 
     //Didn't have any time to attempt writing this class
     private MyStack S;
-
     
+    public RPN{
+	S = new MyStack();
+    }
+    
+    public int parse(String str){
+	Scanner scan = new Scanner(str);
+	//for (int x = 0; x < str.length();x++){
+	//  if ((int)str.charAt(x) 
+	//}
+    }
 
 }
